9161eada5ea03e4f72f4ce5941fca200f037e27e
edmac154 <macsource@4md.com.mx>
2026-05-09 00:52:20 -0600

    enterprise setup

--- services/auth-service/src/app.module.ts
+++ services/auth-service/src/app.module.ts
@@ -32,31 +32,13 @@ import { User } from './users/entities/user.entity';
       ) => ({
         type: 'postgres',
 
-        host:
-          configService.get(
-            'DB_HOST',
-          ),
-
-        port: Number(
-          configService.get(
-            'DB_PORT',
-          ),
+        url: configService.get(
+          'DATABASE_URL',
         ),
 
-        username:
-          configService.get(
-            'DB_USERNAME',
-          ),
-
-        password:
-          configService.get(
-            'DB_PASSWORD',
-          ),
-
-        database:
-          configService.get(
-            'DB_DATABASE',
-          ),
+        ssl: {
+          rejectUnauthorized: false,
+        },
 
         entities: [User],
 
